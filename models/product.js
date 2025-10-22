ddb4205e2c5e4f49f0af31f533e31023bdeab19d
dilshansethmina2002 <132134098+dilshansethmina2002@users.noreply.github.com>
2025-10-22 22:59:27 +0530

    fix: correct stocks field definition in product schema

--- models/product.js
+++ models/product.js
@@ -34,7 +34,7 @@
         images : {
             type : [String],
             default : ["https://www.freepik.com/free-vector/bird-colorful-gradient-design-vector_35322171.htm#fromView=keyword&page=1&position=0&uuid=2a779423-1c2d-4cb1-bbf1-9a0a8af445e6&query=Free+logo+png"]
-        },
+        }, 
         stocks : {
             type : Number,
             required : true,
